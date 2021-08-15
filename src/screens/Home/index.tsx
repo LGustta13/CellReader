c53c5ca2f33b4e117793e857c9d6ca06dc910e3e
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2021-08-14 23:52:29 -0300

    Initialized Homepage with styled-components and custom fonts

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    Container,
+    Header,
+    Content,
+    Footer,
+    NavigationTab
+} from './styles'
+
+
+export function Home(){
+    return(
+        <Container>
+            <Header>
+
+            </Header>
+
+            <Content>
+
+            </Content>
+
+            <Footer>
+
+            </Footer>
+
+            <NavigationTab>
+                
+            </NavigationTab>
+        </Container>
+    )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+
+`;
+
+export const Header = styled.View`
+
+`;
+
+export const Options = styled.View`
+
+`;
+
+export const Content = styled.View`
+
+`;
+
+export const Collection = styled.View`
+
+`;
+
+export const Footer = styled.View`
+
+`;
+
+export const NavigationTab = styled.View`
+
+`;
