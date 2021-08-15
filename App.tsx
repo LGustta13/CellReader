b3359b9ec386754e0fb8ad36a2c17f16562467e1
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2021-08-14 23:56:59 -0300

    Added a ThemeProvider context to use global themes and load fonts

--- App.tsx
+++ App.tsx
@@ -1,19 +1,31 @@
+// CORE
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text} from 'react-native';
 
-export default function App() {
+// COMMUNITY
+import {useFonts} from 'expo-font';
+import {ThemeProvider} from 'styled-components/native';
+import AppLoading from 'expo-app-loading';
+
+// MINE
+import theme from './src/global/styles/theme';
+
+
+export default function App(){
+
+  const [fontsLoaded] = useFonts({
+    'SFPro-SemiBold': require('./src/assets/fonts/SFProDisplaySemiBold.ttf')
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    <ThemeProvider theme={theme}>
+      <Text style={{fontFamily: theme.fonts.semibold, marginTop: 50, fontSize: 100}}>
+        Display
+      </Text>
+    </ThemeProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
